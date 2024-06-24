5e281aa0dd0b28c7b0f85cf7ebda87438665684d
Alk32 <anne_lise.knecht@utt.fr>
2024-06-24 17:26:32 +0200

    Modifications améliorations MVC

--- app/view/patrimoine/viewAmeliorationMVC.php
+++ app/view/patrimoine/viewAmeliorationMVC.php
@@ -1,42 +1,58 @@
 <!-- ----- debut viewAmeliorationMVC -->
 <?php
-
-require ($root . '/app/view/fragment/fragmentPatrimoineHeader.html');
+require($root . '/app/view/fragment/fragmentPatrimoineHeader.html');
 ?>
 
 <body>
   <div class="container">
-      <?php
-      include $root . '/app/view/fragment/fragmentPatrimoineMenu.php';
-      include $root . '/app/view/fragment/fragmentPatrimoineJumbotron.html';
-      ?>
+    <?php
+    include $root . '/app/view/fragment/fragmentPatrimoineMenu.php';
+    include $root . '/app/view/fragment/fragmentPatrimoineJumbotron.html';
+    ?>
 
     <div class="row">
       <div class="col-md-12">
-        <h2>Amélioration du code MVC vu en cours :</h2>
-        <p>
-            L'amélioration que nous proposons est d'ajouter une table 'recompense' qui répertorie des 
-            récompenses attribuées à certaines récoltes.
-        </p>
-        <p>
-            La table sera comme suit : recompense (competition, producteur_id, vin_id, grade)
-        </p>
-        <p>
-            Les attributs producteur_id et vin_id sont nécessaires pour identifier la récolte
-            récompensée. La clé primaire est (competition, vin_id, producteur_id) car une même 
-            récolte peut avoir deux récompenses de deux compétitions différentes.
-            Le grade correspond à 'or', 'argent' ou 'bronze'.
-        </p>
-        <p>
-            On connais tous les médailles sur nos bouteilles de vins. En
-            ajoutant cette table on pourrait aussi proposer une page 'podium' sur notre site
-            pour classer les producteurs en fonction du nombre de récompenses qu'ils ont obtenues.          
-        </p>
+          <h2>Amélioration du code MVC vu en cours :</h2><br> 
+        <p>Afin de limiter les redondances de code dans notre modèle MVC, nous pourrions utiliser des méthodes plus génériques pour les fonctions de bases des modèles.</p>
+        <p>En effet, nous avons remarqué que beaucoup de fonctions se retrouvaient dans chaque modèle, comme les fonctions de création et de lecture par exemple.</p>
+        <p>Nous pourrions donc créer une méthode générique pour chacune de ses fonctions dans le fichier Model.php et faire hériter nos modèles de ces méthodes.</p>
+        <p>Par exemple, pour la lecture, une méthode générale serait :</p>
+        
+        <pre><code class="language-php"><?php echo htmlspecialchars('<?php
+class Model {
+    protected static $table;
+
+    public static function read($id) {
+        $database = self::getInstance();
+        $query = "SELECT * FROM " . static::$table . " WHERE id = :id";
+        $statement = $database->prepare($query);
+        $statement->execute([\'id\' => $id]);
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>'); ?></code></pre>
+        
+        <p>Ainsi, pour notre modèle résidences, il suffirait de deux lignes pour faire l'appel :</p>
+        
+        <pre><code class="language-php"><?php echo htmlspecialchars('<?php
+class ModelResidence extends Model {
+    protected static $table = \'residence\';
+}
+?>'); ?></code></pre>
       </div>
     </div>
-         
   </div>
+
   <?php include $root . '/app/view/fragment/fragmentPatrimoineFooter.html'; ?>
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/themes/prism.min.css" rel="stylesheet" />
+  <style>
+    pre[class*="language-"] {
+      background: none;
+    }
+  </style>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/prism.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.25.0/components/prism-php.min.js"></script>
 </body>
 
 <!-- ----- fin viewAmeliorationMVC -->
